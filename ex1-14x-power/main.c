7d5abe40a06c030c9621977f65ec1e5f4e55ddaf
PedroZappa <pedrogzappa@gmail.com>
2023-08-21 20:05:56 +0100

    ex1-14x-power

---
+++ ex1-14x-power/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int power(int x, int n);
+
+/* Test power() */
+int main(void)
+{
+	int i = 0;
+
+	printf("2^i		2^i		-3^i\n");
+	printf("------------------------------------\n");
+	for (i = 0; i < 10; ++i)
+		printf("%3d		%3d	      %6d\n", i, power(2, i), power(-3, i));
+	return 0;
+}
+
+/* power() : x^n for n >= 0 */
+int power(int base, int n)
+{
+	int i, p;
+
+	p = 1;
+	for (i = 1; i <= n; ++i)
+		p = p * base;
+	return p;
+}

57d91b156e035118615623be30a6a46729fb6602
PedroZappa <pedrogzappa@gmail.com>
2023-08-21 14:33:59 +0100

    ex1-13-word-len-histo : words counting, needs debug

--- ex1-13-word-len-histo/main.c
+++ ex1-13-word-len-histo/main.c
@@ -1,40 +1,40 @@
 #include <stdio.h>
 
+#define WORD_MAX 100
 #define IN 1
 #define OUT 0
 
 int main(void)
 {
-	int c, i, j, c_count, w_count, status;
-	int n_words[100];	
+	int c, i, j, w_len, w_count, status, n_words[WORD_MAX];
 	
-	c_count = w_count = status = i = 0;
+	status = OUT;
+	w_len = w_count = i = 0;
 	while ((c = getchar()) != EOF) 
 	{
-		++c_count;
+		++w_len;
 		if (c == ' ' || c == '\n' || c == '\t')
+		{
 			status = OUT;
+		    n_words[i] = w_len;
+		    w_len = 0;
+		}
 		else if (status == OUT)
 		{
 			status = IN;
 		    ++w_count;
-		    n_words[i] = c_count;
-		    c_count = 0;
 		}
 		++i;
 	}
-	
-	for (i = 0; i < w_count; ++i)
-		n_words[i] = c_count;
 
 	printf("words to print = %d\n", w_count);
-	printf("word length = \n");
+	printf("Word | Length\n");
 
-	/*for (i = 0; i < n_words[w_count]; ++i)
+	for (i = 0; i < n_words[w_count]; ++i)
 	{						
-		printf("%2d ", n_words[i]);
-		for (j = 0; j < c_count; ++j)
+		printf("%3d ", n_words[i]);
+		for (j = 0; j < i; ++j)
 		    printf("*");
 		printf("\n");
-	}*/
+	}
 }

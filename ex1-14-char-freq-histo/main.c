e25e97364a369619eee5f92d4f803f03bce306a3
PedroZappa <pedrogzappa@gmail.com>
2023-08-21 19:47:31 +0100

    ex1-14-char-freq-histo : histogram implemented

--- ex1-14-char-freq-histo/main.c
+++ ex1-14-char-freq-histo/main.c
@@ -3,16 +3,18 @@
 /* Program to print histogram of ASCII char frequencies */
 int main(void)
 {
-	int c, cn, i; // Init Iterators
+	int c, cn, i, star; // Init Iterators
 	int ascii[128] = {0}; // Init Array to store range of ASCII Values
 
+	cn = 0;
 	while ((c = getchar()) != EOF)
 	{
 		++cn;
 		ascii[c] += 1;
 	}
 
-	printf("Input Char Frequency: %d\n", cn);
+	printf("Input Char Frequency\n");
+	printf("Total: %d\n", cn);
 	// Print Histogram
 	for (i = 0; i < 128; ++i)
 	{
@@ -20,15 +22,36 @@ int main(void)
 		{
 			// Print counted ASCII Values
 			if (i == '\t')
-				printf("\\t: %d\n", ascii[i]);
-			if (i == '\n')
-				printf("\\n: %d\n", ascii[i]);
-			if (i == ' ')
-				printf("SPACE: %d\n", ascii[i]);
-			if (i > 32 && i <= 126)
-				printf("%c: %d\n", i, ascii[i]);
-			if (i == 127)
-				printf("DEL: %d\n", ascii[i]);
+		    {
+				printf("\\t: ");
+			    for (star = 0; star < ascii[i]; ++star)
+				    printf("*");
+		    }
+		    if (i == '\n')
+		    {
+				printf("\\n: ");
+				for (star = 0; star < ascii[i]; ++star)
+					printf("*");
+			}
+		    if (i == ' ')
+		    {
+				printf("' ': ");
+				for (star = 0; star < ascii[i]; ++star)
+					printf("*");
+			}
+			if (i > 32 && i <= 126) 
+		    {
+				printf("%c: ", i);
+				for (star = 0; star < ascii[i]; ++star)
+					printf("*");
+			}
+		    if (i == 127)
+			{
+				printf("DEL: ");
+				for (star = 0; star < ascii[i]; ++star)
+					printf("*");
+			}	
+		    printf("\n");						
 		}
 	}
 }

d839916af05193674d12de009afb370d71484206
PedroZappa <pedrogzappa@gmail.com>
2023-08-25 19:07:33 +0100

    ex1-21-entab/ : entab() created, output tabs not counting but output looks fine, needs more testing

--- Ch01/ex1-21-entab/main.c
+++ Ch01/ex1-21-entab/main.c
@@ -5,6 +5,9 @@
 #define MAXLINE		256
 #define MAX			128
 #define TAB_SIZE	8
+#define TAB			'\t'
+
+int entab(char line[], int len);
 
 int	main(void)
 {
@@ -38,34 +41,9 @@ int	main(void)
 	}
 
 	// Entab lines
-	for (i = 0; i < sub_line; i++)
+	for (i = 0; i < sub_line; ++i)
 	{
-		j = space_c = 0;
-		while (lines[i][j] != '\0')
-		{
-		    // Count spaces till count == TAB_SIZE
-			if (lines[i][j] == ' ')
-				space_c++;				// Increment Space Counter
-			if (lines[i][j] != ' ')
-				space_c = 0;			// Reset Counter
-			if (space_c == TAB_SIZE)
-			{
-				/* Count back (TAB_SIZE - 1) spaces */
-				j -= (TAB_SIZE - 1);
-				lines_len[i] -= (TAB_SIZE - 1);
-				lines[i][j] = '\t';		// Replace with TAB
-				space_c = 0;
-				
-				// Remove trailing spaces
-				for (j = (i + 1); j < lines_len[i]; j++)
-				{
-				    lines[i][j] = lines[i][j + (TAB_SIZE - 1)];
-				}
-				space_c = 0;
-				lines[i][lines_len[i]] = '\0';	// Null-terminate
-			}
-			j++;
-		}
+		entab(lines[i], lines_len[i]);
 	}
 
 	// Get Stats & Print lines
@@ -97,4 +75,33 @@ int	main(void)
 	printf("123456789x123456789x123456789x\n");
 
 	return (0);
+}
+
+int entab(char line[], int len)
+{
+	int i, j, k;			// Iterators 
+	int t;					// Toggle
+	int space_c;			// Space Counters 
+
+		space_c = 0;
+		if (line[i] == ' ')
+			space_c++;				// Increment space Counter
+		if (line[i] != ' ')
+			space_c = 0;			// Reset space Counter
+		if (space_c == TAB_SIZE)
+		{
+			
+			i -= (TAB_SIZE - 1);	// Count back to the first blank
+			line[i] = '\t';			// Replace first blank with TAB
+
+			for (j = (i + 1); j < len; j++)
+				line[j] = line[j + (TAB_SIZE - 1)];		// Remove trailing spaces
+			len -= (TAB_SIZE - 1);						// Update line length
+			line[len] = '\0';							// Null-terminate line
+			space_c = 0;								// Reset space counter
+		}
+		++i;
+	
+
+	return (i);
 }

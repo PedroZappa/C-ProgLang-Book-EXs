7ee9681af0597917d3c8baef2ad3918119c9f571
PedroZappa <pedrogzappa@gmail.com>
2023-08-28 21:56:29 +0100

    ex2-05x-getbits : More Studying & Tested

--- Ch02/ex2-05x-getbits/main.c
+++ Ch02/ex2-05x-getbits/main.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define BYTES	 2
+
 unsigned getbits(unsigned x, int p, int n);
 unsigned extended_getbits(unsigned x, int p, int n);
+void print_binary(unsigned x, int size);
 
 int main(int argc, char *argv[])
 {
@@ -17,21 +20,26 @@ int main(int argc, char *argv[])
 		x = atoi(argv[1]);
 
 	// Print UI
-	printf("getbits(	x	p	n	)\n");
+	printf("getbits( x, p, n )\n");
+	printf("x = %u = ", x);
+	print_binary(x, BYTES);
 	printf("====================================================\n");
 
 	z = 0;	
 	max_p = 9; 
-	max_n = 9;
+	max_n = 7;
 	for (p = 0; p < max_p; p++)
     {
 		for (n = 1; n < max_n; n++)
 		{
 			z = getbits(x, p, n);
-			printf("getbits(	%u,	%d,	%d	) = %u (0x%X)\n", x,  p, n, z, z);	
+			printf("getbits( %u, %d, %d ) => %4u = ", x,  p, n, z);
+			print_binary(z, BYTES);
 		}
 		printf("====================================================\n");
-		printf("getbits(	x	p	n	)\n");
+		printf("getbits( x p n )\n");
+		printf("x = %u = ", x);
+		print_binary(x, BYTES);
     }
 
 	return 0;
@@ -49,4 +57,20 @@ unsigned extended_getbits(unsigned x, int p, int n)
 	unsigned x_right = (x >> (p+1-n));	// shift right to make required bits right-justified
 	unsigned mask = ~(~0 << n);			// create 'n'-bit mask
 	return (x_right & mask);			// return required bits
+}
+
+void print_binary(unsigned x, int size)
+{
+	int i;
+
+	for (i = (size * 8) - 1; i >= 0; i--)
+	{
+		if (getbits(x, i, 1) == 1)
+			printf("1");
+		else
+			printf("0");
+		if (i % 4 == 0)
+			printf(" ");
+	}
+	printf("\n");
 }

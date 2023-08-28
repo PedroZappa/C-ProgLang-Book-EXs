e6e859056751abfd1b70afb02b490f3f1e16ff2d
PedroZappa <pedrogzappa@gmail.com>
2023-08-28 10:03:16 +0100

    ex2-05x-getbits : Studied & Tested

--- Ch02/ex2-05x-getbits/main.c
+++ Ch02/ex2-05x-getbits/main.c
@@ -1,28 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 unsigned getbits(unsigned x, int p, int n);
 unsigned extended_getbits(unsigned x, int p, int n);
 
-int main(void)
+int main(int argc, char *argv[])
 {
 	unsigned x = 42;
-	int n;			// 'n' bit field to get
-	int p;			// Beginning at position 'p'
-	int i, j;		// Loop Thresholds
-	int z;
+	int n;				// 'n' bit field to get
+	int p;				// Beginning at position 'p'
+	int max_p, max_n;	// Loop Thresholds
+	int z;				// to store extracted bits
 
-	printf("\tgetbits(x, p, n)\n");
+	// Handle command-line arguments
+	if (argc > 1)
+		x = atoi(argv[1]);
 
-	z = 0;
-	i = j = sizeof(x) + 1;
-	for (p = 0; p < i; p++)
+	// Print UI
+	printf("getbits(	x	p	n	)\n");
+	printf("====================================================\n");
+
+	z = 0;	
+	max_p = 9; 
+	max_n = 9;
+	for (p = 0; p < max_p; p++)
     {
-		for (n = 0; n < j; n++)
+		for (n = 1; n < max_n; n++)
 		{
 			z = getbits(x, p, n);
-			printf("getbits(%u, %d, %d) = %u (0x%X)\n", x,  p, n, z, z);	
+			printf("getbits(	%u,	%d,	%d	) = %u (0x%X)\n", x,  p, n, z, z);	
 		}
-		printf("======================================\n");
+		printf("====================================================\n");
+		printf("getbits(	x	p	n	)\n");
     }
 
 	return 0;
@@ -31,13 +40,13 @@ int main(void)
 /* get 'n' bits from position 'p' */
 unsigned getbits(unsigned x, int p, int n)
 {
-	return (x >> (p + 1 - n)) & (~0 << n);
+	return (x >> (p+1-n)) & ~(~0 << n);
 }
 
 
 unsigned extended_getbits(unsigned x, int p, int n)
 {
-	unsigned x_right = (x >> (p + 1 - n));	// shift right to make required bits right-justified
-	unsigned mask = (~0 << n);				// create 'n'-bit mask
-	return (x_right & mask);				// return required bits
+	unsigned x_right = (x >> (p+1-n));	// shift right to make required bits right-justified
+	unsigned mask = ~(~0 << n);			// create 'n'-bit mask
+	return (x_right & mask);			// return required bits
 }

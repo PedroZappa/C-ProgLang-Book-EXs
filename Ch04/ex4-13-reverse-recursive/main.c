5cffcff0bfeff75ed56b6abfce532e71bf892eb6
PedroZappa <pedrogzappa@gmail.com>
2023-09-08 16:38:26 +0100

    ex4-13-reverse-recursive : cleanup

--- Ch04/ex4-13-reverse-recursive/main.c
+++ Ch04/ex4-13-reverse-recursive/main.c
@@ -3,28 +3,29 @@
 #include <stdio.h>
 #include <string.h>
 
-void reverse_recursive(char s[], int start, int end);
+void reverse_recursive(char str[], int start, int end);
 
 int main () 
 {
     char s[] = "Zedro 42!";
 
-    printf("The string before reverse is : %s\n", s);
+    printf("The string before reverse is : '%s'\n", s);
     reverse_recursive(s, 0, strlen(s)-1);
-    printf("The string after reverse is : %s\n", s);
+    printf("The string after reverse is : '%s'\n", s);
     return (0);
 }
 
 /* reverse_recursive :Recursively reverse the string s */
-void reverse_recursive(char s[], int start, int end)
+void reverse_recursive(char str[], int start, int end)
 {
     char temp;
 
     if (start < end)
     {
-        reverse_recursive(s, start+1, end-1);
-        temp = s[start];
-        s[start] = s[end];
-        s[end] = temp;
+        reverse_recursive(str, start+1, end-1);
+        // Do the swap
+        temp = str[start];
+        str[start] = str[end];
+        str[end] = temp;
     }
 }

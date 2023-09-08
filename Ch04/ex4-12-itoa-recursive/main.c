c82b7f3854200a4ff0d029261b95e95ce64a5b52
PedroZappa <pedrogzappa@gmail.com>
2023-09-08 15:37:36 +0100

    ex4-12-itoa-recursive : DONE & tested; don't print INT_MIN

--- Ch04/ex4-12-itoa-recursive/main.c
+++ Ch04/ex4-12-itoa-recursive/main.c
@@ -1,52 +1,55 @@
 /* Exercise 4.12 : Adapt the ideas of printd to write a recursive 
  * version of itoa ; that is, convert an integer into a string by 
  * calling a recursive routine. */
-#include <stdio.h>
-#include <string.h>
+#include <limits.h>     /* INT_MAX & INT_MIN */
+#include <stdio.h>      /* printf() */
+#include <stdlib.h>     /* abs() */
+#include <string.h>     /* strlen() */
 
 /* Constants */
-#define MAX_STR     100
+#define SEP         "===================================\n"
+#define MAX_LEN    128
 
 /* Function Prototypes */
 void itoa_recursive(int n, char *str, int base);
-void reverse(char *str);
 
 int main(void)
 {
+    char str[MAX_LEN];                            /* to store converted strings */
+    int ints[] = { 1, -7, 42, INT_MAX, INT_MIN };                     /* integers to convert */
+    int ints_n = sizeof(ints) / sizeof(ints[0]);    /* number of integers in array */
+    int i;                                          /* loop counter */
+
+    /* Render Tests*/
+    printf("\tatoi_recursive()\n" SEP); 
+    printf("Integers converted into strings:\n");
+    for (i = 0; i < ints_n; ++i)
+    {
+        itoa_recursive(ints[i], str, 10);
+        printf("%11d\t->\t%s \n", ints[i], str);
+    }
+    printf(SEP);
+    printf("\tUps: Can't print MIN_INT\n");
+
     return (0); 
 }
 
+/* itoa_recursive : convert an integer into a string recursively */
 void itoa_recursive(int n, char *str, int base)
 {
-    // static int i;
-    // static int sign;
+    static int i;
+    //static int sign;
 
-    // if (n < 0)
-    // {
-    //     n *= -1;
-    //     abs = EOF;
-    // }
-
-    if (n < base)
-        *str++ = n + '0';
-    else
+    i = 0;
+    if (n < 0)
     {
-        itoa_recursive(n / base, str, base);
-        *str++ = n % base + '0';
+        str[i++] = '-';     /* append minus sign */
+        n = -n;             /* reset sign in anticipation of next non-recursive call */
     }
-}
-
-/* reverse : reverse a string */
-void reverse(char *str)
-{
-    int c = 0;
-    int start, end;
+    if ((n / base) != 0)     /* recursive call */
+        itoa_recursive((n / base), str, base);
     
-    for (start = 0, end = strlen(str)-1; start < end; start++, end--)
-    {
-        c = str[start];
-        str[start] = str[end];
-        str[end] = c;
-    }
-
+    str[i++] = abs(n % base) + '0';     /* append digit */
+    str[i] = '\0';                      /* terminate string */
 }
+

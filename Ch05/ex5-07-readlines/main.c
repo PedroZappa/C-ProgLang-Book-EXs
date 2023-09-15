ed50502881a1f6f3fcfd2cc0a8372494401d6ce7
PedroZappa <pedrogzappa@gmail.com>
2023-09-15 18:57:22 +0100

    ex5-07-readlines/ : Tested & Annotated

--- Ch05/ex5-07-readlines/main.c
+++ Ch05/ex5-07-readlines/main.c
@@ -25,13 +25,13 @@ int main()
     int nlines;             /* number of input lines */
 
     /* Render Title */
-    printf(SEP "\tft_readlines()\n" SEP);
+    printf(SEP "Lines to sort: \n" SEP);
 
     if ((nlines = ft_readlines(lineptr, MAXLINES)) >= 0)
     {
-        printf(SEP);
-        qsort(lineptr, 0, nlines-1);
-        writelines(lineptr, nlines);
+        qsort(lineptr, 0, nlines-1);        /* Sort lines */
+        printf(SEP "Sorted lines:\n" SEP);  /* Print Header */
+        writelines(lineptr, nlines);        /* Print lines */
         return (0);
     }
     else
@@ -87,19 +87,28 @@ void writelines(char *lineptr[], int nlines)
         printf("%s\n", *lineptr++);
 }
 
-/* qsort() - quick sort */
+/* qsort() - quick sort 
+ * *lineptr[] : is the array of pointers to char;
+ * left & right : are the start and end indices of the array
+ * to be sorted;
+ * */
 void qsort(char *lineptr[], int left, int right)
 {
     int i, last;
 
-    if (left >= right)
-        return;
+    if (left >= right)  /* do nothing if array is empty */
+        return;         
+    /* swap the first and middle elements */
+    /* ((left+right)/2) selects pivot for quicksort */
     swap(lineptr, left, (left+right)/2);
-    last = left;
+    last = left;    /* store pivot position */ 
+    /* loop through the array */
     for (i = left+1; i <= right; i++)
+        /* if the current element is smaller than the pivot */
         if (strcmp(lineptr[i], lineptr[left]) < 0)
             swap(lineptr, ++last, i);
     swap(lineptr, left, last);
+    /* Recursively sort the two subarrays */
     qsort(lineptr, left, last-1);
     qsort(lineptr, last+1, right);
 }

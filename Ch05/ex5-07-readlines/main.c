ff64b3af5b30f639fe9472bfdf4309f12e64d99e
PedroZappa <pedrogzappa@gmail.com>
2023-09-15 13:05:00 +0100

    ex5-07-readlines/ : readline, writeline and qsort implemented

--- Ch05/ex5-07-readlines/main.c
+++ Ch05/ex5-07-readlines/main.c
@@ -1,9 +1,10 @@
-/* Eercise 5-7. Rewrite readlines to store lines in an array 
+/* Exercise 5-7. Rewrite readlines to store lines in an array 
  * supplied by main, rather than calling alloc to maintain 
  * storage. How much faster is the pro- gram? */ 
 #include <stdio.h>
 #include <string.h>
 
+#define SEP         "==============================\n"
 #define MAXLINES    1024
 #define MAXLEN      512
 
@@ -13,7 +14,7 @@ char lines[MAXLINES][MAXLEN];   /* array of lines */
 
 /* Function Prototypes */
 int ft_getline(char *s, int i);
-int readlines(char *lineptr[], int nlines);
+int ft_readlines(char *lineptr[], int nlines);
 void writelines(char *lineptr[], int nlines);
 
 void qsort(char *lineptr[], int left, int right);
@@ -23,9 +24,12 @@ int main()
 {
     int nlines;             /* number of input lines */
 
-    if ((nlines = readlines(lineptr, MAXLINES)) >= 0)
+    /* Render Title */
+    printf(SEP "\tft_readlines()\n" SEP);
+
+    if ((nlines = ft_readlines(lineptr, MAXLINES)) >= 0)
     {
-        qsort(lineptr, 0, nlines-1);
+        //qsort(lineptr, 0, nlines-1);
         writelines(lineptr, nlines);
         return (0);
     }
@@ -41,17 +45,55 @@ int ft_getline(char *s, int lim)
 {
     int c, i;
 
-    for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; i++)    
+    for (i = 0; i < lim-1 && (c = getchar()) != EOF && c != '\n'; i++)    
         s[i] = c;
     if (c == '\n')
         s[i++] = c;
     s[i] = '\0';
+
     return i;
 }
 
 /* readlines() - read input lines */
-int readlines(char *lineptr[], int maxlines)
+int ft_readlines(char *lineptr[], int maxlines)
 {
     int len, nlines;
-    char *p;
+
+    nlines = 0;
+    while ((len = ft_getline(lineptr[nlines], MAXLEN)) > 0)
+    {
+        if (nlines >= maxlines)
+            return -1;
+        else
+        {
+            lines[nlines][len-1] = '\0';        /* Delete \n */
+            lineptr[nlines] = lines[nlines];    /**/ 
+            nlines++;                           /* Increment nlines */
+        }
+    }
+    return (nlines);
+}
+
+/* writelines() - write output lines */
+void writelines(char *lineptr[], int nlines)
+{
+    while (nlines-- > 0)
+        printf("%s\n", *lineptr++);
+}
+
+/* qsort() - quick sort */
+void qsort(char *lineptr[], int left, int right)
+{
+    int i, last;
+
+    if (left >= right)
+        return;
+    swap(lineptr, left, (left+right)/2);
+    last = left;
+    for (i = left+1; i <= right; i++)
+        if (strcmp(lineptr[i], lineptr[left]) < 0)
+            swap(lineptr, ++last, i);
+    swap(lineptr, left, last);
+    qsort(lineptr, left, last-1);
+    qsort(lineptr, last+1, right);
 }

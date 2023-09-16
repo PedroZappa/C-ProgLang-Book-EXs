12c5e9980577f5f31f36d72c44c35c3a0b35d460
PedroZappa <pedrogzappa@gmail.com>
2023-09-16 22:59:20 +0100

    ex5-11a-entab : Wrote pseudo-code for the implementation in annotations

--- Ch05/ex5-11a-entab/main.c
+++ Ch05/ex5-11a-entab/main.c
@@ -19,14 +19,42 @@ void sort_args(int[], int n);
 /* Entab Driver */
 int main(int argc, char *argv[])
 {
-    int c;  
-    int i;                  /* Tab Index Iterator */ 
-    int nxt_ts;             /* Next tab stop */
-    int nxt_def_ts;         /* Next default tab stop */
+    int c;                  /* Current input char */
     int col;                /* Column index */
     int spaces;             /* Number of spaces ... */
-    int tab_c[MAX_TABS];
+    int tabs[MAX_TABS];     /* Array of tab stop lengths */
+    int tabs_i;             /* tabs[] index Iterator */ 
+    int tab_base;           /* Base tab stop */
+    int nxt_ts;             /* Next tab stop */
+    int nxt_def_ts;         /* Next default tab stop */
 
+    /* Parse user input */
+    /* Loop through args */
+        /* return if too many args */
+        /* filter for ints with atoi() */
+            /* store args in tabs[] */
+    
+    /* If there tabs[] has values */
+        /* Sort the array */
+        /* Get largest value into tab_base */
+        /* Add the TAB formula to tab_base */
+        /* Divide tab_base by DEF_TABSTOP */ 
+
+
+    /* Get lines */ 
+        /* for every char that is not a space */
+            /* */
+
+        /* switch through every char */
+            /* Count spaces */
+            /* If \n reset col and tabs_i & print char */
+            /* If tab ... */
+                /* Find next custom tab */
+                /* if next custum tab is not too big */
+                    /* subtract current col from custom tab store it in nxt_ts */
+                /* add spaces to nxt_ts */
+                
+            /* Default, print char */
 
 
     return 0;
